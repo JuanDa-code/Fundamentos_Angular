6c45b67435cf1c35cca0acfe85f7fd7f4dfe43a5
JuanDa-code <juandax123456@gmail.com>
2023-05-24 13:17:24 -0500

    NgClass y NgStyle

--- my-store/src/app/app.component.ts
+++ my-store/src/app/app.component.ts
@@ -20,6 +20,12 @@ export class AppComponent {
   names: string[] = ['Nicolas', 'Juan', 'Julian'];
   newName = '';
 
+  box = {
+    width: 100,
+    height: 100,
+    background: 'red',
+  };
+
   products: Product[] = [
     {
       name: 'EL mejor juguete',
