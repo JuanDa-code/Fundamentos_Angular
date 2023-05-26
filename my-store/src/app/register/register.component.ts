549d4aea5f83b4a3601b9520975e103f651fd1f4
JuanDa-code <juandax123456@gmail.com>
2023-05-26 15:03:57 -0500

    Formulario de register

--- my-store/src/app/register/register.component.ts
+++ my-store/src/app/register/register.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.scss']
 })
-export class LoginComponent {
-  loginForm = {
+export class RegisterComponent {
+  registerForm = {
+    name: '',
     email: '',
     password: '',
   }
@@ -25,11 +26,11 @@ export class LoginComponent {
     }
   ]
 
-  onLogin() {
+  onRegister() {
     this.users.forEach(user => {
-      if (user.email === this.loginForm.email && user.password === this.loginForm.password) {
+      if (user.email === this.registerForm.email && user.password === this.registerForm.password) {
         if (this.messageError != '' || this.isInvalid != '') {
-          console.log(this.loginForm);
+          console.log(this.registerForm);
         }
       } else {
         this.messageError = 'Invalid email and password combination.';
